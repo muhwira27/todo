ca120495065fb7896714929310968f60275f6690
Muh. Wira <muhwira907@gmail.com>
2022-07-06 15:18:40 +0800

    update database.ts

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,23 @@
 import { Pool } from "pg";
+import { Generated, Kysely, PostgresDialect } from "kysely";
 
-export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: '',
-    database: 'todo',
-    port: 3306
-})
+interface todoTable {
+    id: Generated<number>
+    description: string
+    done: boolean
+}
+
+interface Database {
+    todos: todoTable
+}
+
+export const db = new Kysely<Database>({
+    dialect: new PostgresDialect({
+      pool: new Pool({
+          user: 'postgres',
+          host: 'localhost',
+          database: 'todo',
+          port: 3306,
+      })
+    }),
+  })
